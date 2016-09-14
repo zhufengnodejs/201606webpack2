5770c015f8d370b9f6d01a18ab31b6b34ef522be
zhufengnodejs <zhufengnodejs@126.com>
2016-09-14 17:16:46 +0800

    从数据库中初始化数据

--- comment/components/CommentBox.js
+++ comment/components/CommentBox.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import CommentForm from './CommentForm';
 import CommentList from './CommentList';
+import $ from 'jquery';
 export default class CommentBox extends React.Component{
     //构造函数 使用属性对象调用父类的构造函数进行初始化
     constructor(props){
         super(props);
         //es5  getInitialState() return {comments:[]}
         //es6
-        this.state = {comments:[
-            {name:'张三',date:'2016',content:'今天天气不错'}
-            ,{name:'李四',date:'2017',content:'明天天气不错'}
-        ]};
+        this.state = {comments:[]};
+        this.init();
     }
-
+    init(){
+        $.get(this.props.url).then(comments=>{
+            this.setState({comments});
+        })
+    }
     render(){
         return (
             <div className="container">
--- comment/server.js
+++ comment/server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var db = require('./db');
 var app = express();
+app.use(function(req,res,next){
+    res.setHeader('Access-Control-Allow-Origin',"*");
+    next();
+});
+//查询所有的留言
 app.get('/comments',function(req,res){
     db.Comment.find().then(function(comments){
         res.send(comments);
