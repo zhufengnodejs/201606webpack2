c24e3fc88049938cc6465bd240aeb96e5374e26a
zhufengnodejs <zhufengnodejs@126.com>
2016-09-14 13:07:15 +0800

    实现预览

--- react/components/Form.js
+++ react/components/Form.js
@@ -1,22 +1,16 @@
 import React from 'react';
 
 export default React.createClass({
-    setUsername(event){
-        this.props.setVal('username',event.target.value);
-    },
-    setPassword(event){
-        this.props.setVal('password',event.target.value);
-    },
     render(){
         return (
             <form>
                 <div className="form-group">
                     <label htmlFor="username">用户名</label>
-                    <input onChange={this.setUsername}  value={this.props.username} type="text" className="form-control"/>
+                    <input onChange={(event)=>{this.props.setVal('username',event.target.value)}}  value={this.props.username} type="text" className="form-control"/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">密码</label>
-                    <input onChange={this.setPassword} value={this.props.password} type="text" className="form-control"/>
+                    <input onChange={(event)=>{this.props.setVal('password',event.target.value)}}  value={this.props.password} type="text" className="form-control"/>
                 </div>
             </form>
         )
