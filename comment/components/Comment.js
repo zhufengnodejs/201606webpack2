64d6ffa33240f40e7ebaf6d675e5161b9c8766e8
zhufengnodejs <zhufengnodejs@126.com>
2016-09-14 18:08:17 +0800

    请大家自行删除删除功能

--- comment/components/Comment.js
+++ comment/components/Comment.js
@@ -6,9 +6,16 @@ export default class Comment extends React.Component{
         super(props);
     }
     render(){
+        /**
+         * 1. 给小X绑定点击事件,取得当前要删除的留言ID
+         * 2. 调用CommentBox的删除方法，调用后台接口进行删除
+         * 3. 删除之后重新刷新留言列表
+         */
         return (
             <li className="list-group-item">
-                {this.props.name}:{this.props.children}<span className="pull-right">{
+                {this.props.name}:{this.props.children}
+                <span className="glyphicon glyphicon-remove"></span>
+                <span className="pull-right">{
                 moment(this.props.date).fromNow()}</span>
             </li>
         )
